f42a47cd755f57f3eb4e2ebf689945e4e327961a
Henry Ricaurte Mora <hricaurtem@proton.me>
2025-01-19 11:45:58 -0500

    Solo añadí otra cosa para la busqueda

--- PowerDesigner/ModuleCrebas.sql
+++ PowerDesigner/ModuleCrebas.sql
@@ -1,6 +1,6 @@
 /*==============================================================*/
 /* DBMS name:      ORACLE Version 11g                           */
-/* Created on:     16/01/2025 10:16:34 p. m.                    */
+/* Created on:     19/01/2025 10:54:19 a. m.                    */
 /*==============================================================*/
 
 
@@ -13,6 +13,9 @@ alter table ARCHIVOADJUNTO
 alter table CONTACTO
    drop constraint FK_CONTACTO_CONTACTOS_USUARIO;
 
+alter table CONTACTO
+   drop constraint FK_CONTACTO_USUARIO_Q_USUARIO;
+
 alter table DESTINATARIO
    drop constraint FK_DESTINAT_CONTACTOD_CONTACTO;
 
@@ -24,6 +27,9 @@ alter table DESTINATARIO
 
 alter table DESTINATARIO
    drop constraint FK_DESTINAT_TIPOCOPIA_TIPOCOPI;
+
+alter table MENSAJE
+   drop constraint FK_MENSAJE_CATEGORIA_CATEGORI;
 
 alter table MENSAJE
    drop constraint FK_MENSAJE_MENSAJESU_MENSAJE;
@@ -43,14 +49,15 @@ alter table USUARIO
 alter table USUARIO
    drop constraint FK_USUARIO_PAISUSUAR_PAIS;
 
-alter table USUARIO
-   drop constraint FK_USUARIO_USUARIO_Q_CONTACTO;
-
 drop index TIPOARCHIVO_ARCHIVOADJUNTO_FK;
 
 drop index MENSAJE_ARCHIVOADJUNTO_FK;
 
 drop table ARCHIVOADJUNTO cascade constraints;
+
+drop table CATEGORIA cascade constraints;
+
+drop index USUARIO_QUE_TAMBIEN_ES2_FK;
 
 drop index CONTACTOS_DEL_USUARIO_FK;
 
@@ -76,6 +83,8 @@ drop index PAISMENSAJE_FK;
 
 drop index MENSAJESUP_FK;
 
+drop index CATEGORIAMENSAJE_FK;
+
 drop table MENSAJE cascade constraints;
 
 drop table PAIS cascade constraints;
@@ -85,8 +94,6 @@ drop table TIPOARCHIVO cascade constraints;
 drop table TIPOCARPETA cascade constraints;
 
 drop table TIPOCOPIA cascade constraints;
-
-drop index USUARIO_QUE_TAMBIEN_ES_FK;
 
 drop index PAISUSUARIO_FK;
 
@@ -123,12 +130,23 @@ create index TIPOARCHIVO_ARCHIVOADJUNTO_FK on ARCHIVOADJUNTO (
 );
 
 /*==============================================================*/
+/* Table: CATEGORIA                                             */
+/*==============================================================*/
+create table CATEGORIA 
+(
+   IDCATEGORIA          VARCHAR2(3)          not null,
+   DESCCATEGORIA        VARCHAR2(30)         not null,
+   constraint PK_CATEGORIA primary key (IDCATEGORIA)
+);
+
+/*==============================================================*/
 /* Table: CONTACTO                                              */
 /*==============================================================*/
 create table CONTACTO 
 (
    CONSECCONTACTO       INTEGER              not null,
    USUARIO              VARCHAR2(5)          not null,
+   USU_USUARIO          VARCHAR2(5)          not null,
    NOMBRECONTACTO       VARCHAR2(30)         not null,
    CORREOCONTACTO       VARCHAR2(30)         not null,
    constraint PK_CONTACTO primary key (CONSECCONTACTO)
@@ -142,6 +160,13 @@ comment on table CONTACTO is
 /*==============================================================*/
 create index CONTACTOS_DEL_USUARIO_FK on CONTACTO (
    USUARIO ASC
+);
+
+/*==============================================================*/
+/* Index: USUARIO_QUE_TAMBIEN_ES2_FK                            */
+/*==============================================================*/
+create index USUARIO_QUE_TAMBIEN_ES2_FK on CONTACTO (
+   USU_USUARIO ASC
 );
 
 /*==============================================================*/
@@ -211,6 +236,7 @@ create table MENSAJE
    IDPAIS               VARCHAR2(5)          not null,
    MEN_USUARIO          VARCHAR2(5),
    MEN_IDMENSAJE        VARCHAR2(5),
+   IDCATEGORIA          VARCHAR2(3)          not null,
    ASUNTO               VARCHAR2(255)        not null,
    CUERPOMENSAJE        VARCHAR2(255)        not null,
    FECHAACCION          DATE                 not null,
@@ -220,6 +246,13 @@ create table MENSAJE
 
 comment on table MENSAJE is
 'Almancena todos los mensajes';
+
+/*==============================================================*/
+/* Index: CATEGORIAMENSAJE_FK                                   */
+/*==============================================================*/
+create index CATEGORIAMENSAJE_FK on MENSAJE (
+   IDCATEGORIA ASC
+);
 
 /*==============================================================*/
 /* Index: MENSAJESUP_FK                                         */
@@ -305,7 +338,6 @@ create table USUARIO
    USUARIO              VARCHAR2(5)          not null,
    IDESTADO             VARCHAR2(5)          not null,
    IDPAIS               VARCHAR2(5)          not null,
-   CONSECCONTACTO       INTEGER,
    NOMBRE               VARCHAR2(30)         not null,
    APELLIDO             VARCHAR2(30)         not null,
    FECHANACIMIENTO      DATE                 not null,
@@ -332,13 +364,6 @@ create index PAISUSUARIO_FK on USUARIO (
    IDPAIS ASC
 );
 
-/*==============================================================*/
-/* Index: USUARIO_QUE_TAMBIEN_ES_FK                             */
-/*==============================================================*/
-create index USUARIO_QUE_TAMBIEN_ES_FK on USUARIO (
-   CONSECCONTACTO ASC
-);
-
 alter table ARCHIVOADJUNTO
    add constraint FK_ARCHIVOA_MENSAJE_A_MENSAJE foreign key (USUARIO, IDMENSAJE)
       references MENSAJE (USUARIO, IDMENSAJE);
@@ -349,6 +374,10 @@ alter table ARCHIVOADJUNTO
 
 alter table CONTACTO
    add constraint FK_CONTACTO_CONTACTOS_USUARIO foreign key (USUARIO)
+      references USUARIO (USUARIO);
+
+alter table CONTACTO
+   add constraint FK_CONTACTO_USUARIO_Q_USUARIO foreign key (USU_USUARIO)
       references USUARIO (USUARIO);
 
 alter table DESTINATARIO
@@ -366,6 +395,10 @@ alter table DESTINATARIO
 alter table DESTINATARIO
    add constraint FK_DESTINAT_TIPOCOPIA_TIPOCOPI foreign key (IDTIPOCOPIA)
       references TIPOCOPIA (IDTIPOCOPIA);
+
+alter table MENSAJE
+   add constraint FK_MENSAJE_CATEGORIA_CATEGORI foreign key (IDCATEGORIA)
+      references CATEGORIA (IDCATEGORIA);
 
 alter table MENSAJE
    add constraint FK_MENSAJE_MENSAJESU_MENSAJE foreign key (MEN_USUARIO, MEN_IDMENSAJE)
@@ -390,8 +423,4 @@ alter table USUARIO
 alter table USUARIO
    add constraint FK_USUARIO_PAISUSUAR_PAIS foreign key (IDPAIS)
       references PAIS (IDPAIS);
-
-alter table USUARIO
-   add constraint FK_USUARIO_USUARIO_Q_CONTACTO foreign key (CONSECCONTACTO)
-      references CONTACTO (CONSECCONTACTO);
 
